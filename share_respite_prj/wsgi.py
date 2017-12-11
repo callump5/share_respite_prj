84b97abc17feb5feb71d8a02b181cae640076419
callump5 <clpullinger@gmail.com>
2017-12-11 12:28:23 +0000

    TO DEPLOY!!

--- share_respite_prj/wsgi.py
+++ share_respite_prj/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "share_respite_prj.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "share_respite_prj.settings.staging")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
