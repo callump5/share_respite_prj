c111c3b65bca65be9965a6d3a403d149d0943f30
callump5 <clpullinger@gmail.com>
2017-12-11 12:25:11 +0000

    TO DEPLOY!!

--- share_respite_prj/urls.py
+++ share_respite_prj/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.static import serve
-from settings.base import MEDIA_ROOT
+from settings.base import MEDIA_ROOT, STATIC_ROOT
 
 from home import views as home_views
 
@@ -25,6 +25,7 @@ urlpatterns = [
 
     # Images
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
 
     # Home Page
     url(r'^$', home_views.get_index, name='home'),
