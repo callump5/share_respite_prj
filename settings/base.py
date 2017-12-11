1e585ba5bd4a4f10cd74404eaedab02915d9931b
callump5 <clpullinger@gmail.com>
2017-12-11 11:31:52 +0000

    TO DEPLOY!!

--- settings/base.py
+++ settings/base.py
@@ -97,10 +97,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'share_respite_prj/static/',  "static"),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'share_respite_prj/media/', 'media')
 MEDIA_URL = '/media/'
 
 # Message Tags
